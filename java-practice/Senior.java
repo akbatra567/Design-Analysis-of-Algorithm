601c8df83ddadd22656b56bea0384532ce4e3cf1
Akshit Batra <59093007+akbatra567@users.noreply.github.com>
2024-08-03 00:29:12 +0530

    Update Senior.java

--- java-practice/Senior.java
+++ java-practice/Senior.java
@@ -32,9 +32,7 @@ https://leetcode.com/problems/number-of-senior-citizens/editorial/?envType=daily
 public class Senior {
 
   public static int countSeniorCitizens(String[] tickets){
-    
     int count = 0;
-
     for(String ticket: tickets){
       String num = ticket.charAt(11) + "" + ticket.charAt(12);
       int age = Integer.parseInt(num);  
@@ -42,9 +40,20 @@ public class Senior {
 	count++;
       }
     }
-
     return count;
   }
+
+//  Alternate -- Much faster
+  public int countSeniors(String[] details) {
+   int count = 0;
+   for (String ticket : details) {
+     int age = Integer.parseInt(ticket.substring(11, 13));
+       if (age > 60) {
+	 count++;
+       }
+     }
+    return count;
+    }
     
     public static void main(String[] args){
       String[] ticketDetails = new String[]{"7868190130M7522","5303914400F9211","9273338290F4010"};
